cf92fa5541c7930f35f82695ab4a6cad0464835a
Sam3l3 <samu2mila@gmail.com>
2024-03-05 13:10:05 +0100

    homepege creation and searchbar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,9 @@
+const SearchBar = ()=> {
+    return (
+        <div className="search-box">
+            <input type="text" className="search-input" placeholder="Search..." />
+
+        </div>
+    )
+}
+export default SearchBar;
---
+++ src/views/HomePage.jsx
@@ -0,0 +1,10 @@
+import SearchBar from '../components/SearchBar';
+
+export const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+        <SearchBar />
+    </div>
+  );
+}
